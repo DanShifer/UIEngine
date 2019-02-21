7b615be98977626aace8c37cfea4a58222af9074
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-02-21 18:42:00 +0500

    Add Generator Computer Id to string

--- Helper/Site/User/Client.cs
+++ Helper/Site/User/Client.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.Management;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace UIEngine.Helper.Site.User
@@ -10,7 +11,7 @@ namespace UIEngine.Helper.Site.User
         /// </summary>
         /// <param name="Data">Данные для криптования</param>
         /// <returns></returns>
-        private static string GetMD5(string Data)
+        public static string GetMD5(string Data)
         {
             string Hash = "";
 
@@ -20,6 +21,33 @@ namespace UIEngine.Helper.Site.User
             }
         
             return Hash.ToUpper();
+        }
+
+        public static string GenerateComputerID()
+        {
+            //Creates the HWID from the ProcessorID, Video Controller RAM and the size of the disk drive ; you can change this of course
+            StringBuilder computerID = new StringBuilder();
+            ManagementObjectSearcher searcher;
+
+            searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                computerID.Append(queryObj["ProcessorId"]);
+            }
+
+            searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_VideoController");
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                computerID.Append(queryObj["AdapterRAM"]);
+            }
+
+            searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_DiskDrive");
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                computerID.Append(queryObj["Size"]);
+            }
+
+            return computerID.ToString();
         }
     }
 }
