8ae2f7334062e54043ecfa8134065f7e53fd6a45
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-02-23 14:04:34 +0500

    Add mouse_event

--- API/UserAPI.cs
+++ API/UserAPI.cs
@@ -7,6 +7,17 @@ namespace UIEngine.API
     public class UserAPI
     {
         /// <summary>
+        /// Cинтезирует движение мыши и щелчки кнопок.
+        /// </summary>
+        /// <param name="dwFlags"></param>
+        /// <param name="dx"></param>
+        /// <param name="dy"></param>
+        /// <param name="dwData"></param>
+        /// <param name="dwExtraInfo"></param>
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData,int dwExtraInfo);
+
+        /// <summary>
         /// Проверка нажатой клавиши
         /// </summary>
         /// <param name="vKey">Клавиша</param>
