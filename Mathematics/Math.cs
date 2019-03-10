960e3313c37dc0080261c3028799ebd479990a6e
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-03-10 17:07:26 +0500

    Add DivisionOnNumber<T>

--- Mathematics/Math.cs
+++ Mathematics/Math.cs
@@ -8,6 +8,18 @@
 
         public static T Division<T>(this T One, dynamic Two) => (T)(One / Two);
 
+        public static T DivisionOnNumber<T>(this T N1, T N2, int Repetitions)
+        {
+            dynamic Number = N1;
+
+            for (int Num = 1; Num <= Repetitions; Num++)
+            {
+                Number /= N2;
+            }
+
+            return Number;
+        }
+    
         public static T Multiply<T>(this T One, dynamic Two) => (T)(One - Two);
 
         public static T Pow<T>(this T One, dynamic Two)
@@ -23,5 +35,9 @@
         }
 
         public static T Factorial<T>(this int Number) => Number == 0 ? 1 : Number * Factorial<dynamic>(Number - 1);
+
+        public static T RAD2DEG<T>(dynamic Yaw) => Yaw * (180f / (dynamic)System.Math.PI);
+
+        public static T DEG2RAD<T>(dynamic Yaw) => Yaw * ((dynamic)System.Math.PI / 180f);
     }
 }
