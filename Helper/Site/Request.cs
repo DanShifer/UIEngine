d1eec395d6c2034b31f96349f31f26b185e6a69c
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-03-17 23:06:02 +0500

    Add Params for HttpWebRequest

--- Helper/Site/Request.cs
+++ Helper/Site/Request.cs
@@ -4,23 +4,28 @@ using System.IO;
 using System.Net;
 using System.Text;
 using UIEngine.Helper.Enum;
+using UIEngine.Helper.Site.Helper;
 
 namespace UIEngine.Helper.Site
 {
     public class Request
     {
+        #region Params
         private readonly string Address = null;
         private readonly RequestMethod RequestMethod;
 
-        private WebRequest WebRequest;
-        private WebResponse WebResponse;
+        private HttpWebRequest HttpWebRequest;
+        private HttpWebResponse HttpWebResponse;
+        private RequestHeader RequestHeader;
 
         private Dictionary<string, string> KeyValuePairs = new Dictionary<string, string>();
+        #endregion
 
-        public Request(string Address, RequestMethod RequestMethod = RequestMethod.POST)
+        public Request(string Address, RequestMethod RequestMethod = RequestMethod.POST, RequestHeader RequestHeader = null)
         {
             this.Address = Address;
             this.RequestMethod = RequestMethod;
+            this.RequestHeader = RequestHeader;
         }
 
         public object this[string Param]
@@ -28,29 +33,60 @@ namespace UIEngine.Helper.Site
             set => KeyValuePairs.Add(Param, value.ToString());
         }
 
+        #region Params
+        public string ContentType
+        {
+            private get;
+            set;
+        }
+
+        public string Accept
+        {
+            private get;
+            set;
+        }
+
+        public string UserAgent
+        {
+            private get;
+            set;
+        }
+        #endregion
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Не ликвидировать объекты несколько раз")]
-        public virtual string GetRespone()
+        public string GetRespone()
         {
             try
             {
                 string ReadResponse = null;
 
-                this.WebRequest = this.RequestMethod == RequestMethod.GET ? WebRequest.Create(this.Address + Encoding.UTF8.GetString(GetParam())) : WebRequest.Create(this.Address);
+                this.HttpWebRequest = this.RequestMethod == RequestMethod.GET ? (HttpWebRequest)WebRequest.Create(this.Address + Encoding.UTF8.GetString(GetParam())) : (HttpWebRequest)WebRequest.Create(this.Address);
 
-                this.WebRequest.Method = this.RequestMethod == RequestMethod.GET ? "GET" : "POST";
-                this.WebRequest.ContentType = "application/x-www-form-urlencoded";
+                this.HttpWebRequest.Method = this.RequestMethod == RequestMethod.GET ? "GET" : "POST";
+
+                if (RequestHeader != null)
+                {
+                    foreach (var Headers in RequestHeader?.KeyValuePairs)
+                    {
+                        this.HttpWebRequest.Headers.Add(Headers.Key, Headers.Value);
+                    }
+                }
+
+                this.HttpWebRequest.ContentType = ContentType;
+                this.HttpWebRequest.Accept = Accept;
+                this.HttpWebRequest.UserAgent = UserAgent;
 
                 if (this.RequestMethod == RequestMethod.POST)
                 {
-                    this.WebRequest.ContentLength = GetParam().Length;
-                    using (Stream StreamRequest = this.WebRequest.GetRequestStream())
+                    this.HttpWebRequest.ContentLength = GetParam().Length;
+                    using (Stream StreamRequest = this.HttpWebRequest.GetRequestStream())
                     {
                         StreamRequest.Write(GetParam(), 0, GetParam().Length);
                     }
                 }
 
-                this.WebResponse = this.WebRequest.GetResponse();
-                using (Stream StreamResponse = WebResponse.GetResponseStream())
+                this.HttpWebResponse = (HttpWebResponse)this.HttpWebRequest.GetResponse();
+                using (Stream StreamResponse = HttpWebResponse.GetResponseStream())
                 {
                     using (StreamReader StreamReader = new StreamReader(StreamResponse))
                     {
@@ -60,7 +96,7 @@ namespace UIEngine.Helper.Site
 
                 return ReadResponse;
             }
-            catch(Exception EX)
+            catch (Exception EX)
             {
                 return EX.Message;
             }
