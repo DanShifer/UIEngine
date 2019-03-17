22a01126c14442dd087e18125fd98decd82803e0
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-03-17 23:05:45 +0500

    Add RequestHeader for WebRequest

---
+++ Helper/Site/Helper/RequestHeader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace UIEngine.Helper.Site.Helper
+{
+    public class RequestHeader
+    {
+        public readonly Dictionary<string, string> KeyValuePairs = new Dictionary<string, string>();
+
+        public string this[string Key]
+        {
+            get => KeyValuePairs[Key];
+            set => KeyValuePairs.Add(Key, value);
+        }
+
+        public Dictionary<string,string> this[string Key,string Value]
+        {
+            get => KeyValuePairs;
+            set => KeyValuePairs.Add(Key, Value);
+        }       
+    }
+}
