9b7c74f0fff535a4218beaea28b939692ea78cfd
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-03-18 00:31:34 +0500

    Fix Bug

--- Memory/OldMemory.cs
+++ Memory/OldMemory.cs
@@ -28,51 +28,91 @@ namespace UIEngine.Memory
         ~OldMemory() => KernelAPI.CloseHandle(ProcessHandle);
 
         #region Read
+        /// <summary>
+        /// Convert Read Byte in Boolean
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public bool ReadBoolean(int Address) => Convert.ToBoolean(ReadBytes((IntPtr)Address, sizeof(bool)));
 
+        /// <summary>
+        /// Convert Read Byte in Decimal
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public decimal ReadDecimal(int Address) => Convert.ToDecimal(ReadBytes((IntPtr)Address, sizeof(decimal)));
 
+        /// <summary>
+        /// Convert Read Byte in Double
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public double ReadDouble(int Address) => Convert.ToDouble(ReadBytes((IntPtr)Address, sizeof(double)));
 
+        /// <summary>
+        /// Convert Read Byte in Single
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public float ReadFloat(int Address) => Convert.ToSingle(ReadBytes((IntPtr)Address, sizeof(float)));
 
+        /// <summary>
+        /// Convert Read Byte in Int16
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public short ReadShort(int Address) => Convert.ToInt16(ReadBytes((IntPtr)Address, sizeof(short)));
 
+        /// <summary>
+        /// Convert Read Byte in Int32(DWORD)
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public int ReadInteger32(int Address) => Convert.ToInt32(ReadBytes((IntPtr)Address, sizeof(int)));
 
+        /// <summary>
+        /// Convert Read Byte in Int64
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public long ReadLong(int Address) => Convert.ToInt64(ReadBytes((IntPtr)Address, sizeof(long)));
 
+        /// <summary>
+        /// Convert Read Byte in SByte
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public sbyte ReadSByte(int Address) => Convert.ToSByte(ReadBytes((IntPtr)Address, sizeof(sbyte)));
 
+        /// <summary>
+        /// Convert Read Byte in UInt32
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public uint ReadUint(int Address) => Convert.ToUInt32(ReadBytes((IntPtr)Address, sizeof(uint)));
 
+        /// <summary>
+        /// Convert Read Byte in UInt16
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public ushort ReadUShort(int Address) => Convert.ToUInt16(ReadBytes((IntPtr)Address, sizeof(ushort)));
 
+        /// <summary>
+        /// Convert Read Byte in UInt64
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
         public ulong ReadULong(int Address) => Convert.ToUInt64(ReadBytes((IntPtr)Address, sizeof(ulong)));
         #endregion
 
         #region Write
-        public bool WriteBoolean<T>(int Address,T Value) => WriteBytes((IntPtr)Address,Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteDecimal<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteDouble<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteFloat<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteShort<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteInteger32<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteLong<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteSByte<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteUint<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteUShort<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
-
-        public bool WriteULong<T>(int Address, T Value) => WriteBytes((IntPtr)Address, Encoding.UTF8.GetBytes(Value.ToString()));
+        /// <summary>
+        /// Write Type in Function(Address)
+        /// </summary>
+        /// <param name="Address">Address function</param>
+        /// <returns></returns>
+        public bool WriteTypeEnum<T>(int Address,T Value) => WriteBytes((IntPtr)Address,Encoding.UTF8.GetBytes(Value.ToString()));
         #endregion
     }
 }
