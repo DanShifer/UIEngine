e771c156b298846952869b91665b5697e9159242
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-04-04 18:16:50 +0500

    Rename function

--- Helper/Site/Helper/RequestHeader.cs
+++ Helper/Site/Helper/RequestHeader.cs
@@ -4,18 +4,18 @@ namespace UIEngine.Helper.Site.Helper
 {
     public class RequestHeader
     {
-        public readonly Dictionary<string, string> KeyValuePairs = new Dictionary<string, string>();
+        public readonly Dictionary<string, string> RequestValue = new Dictionary<string, string>();
 
         public string this[string Key]
         {
-            get => KeyValuePairs[Key];
-            set => KeyValuePairs.Add(Key, value);
+            get => RequestValue[Key];
+            set => RequestValue.Add(Key, value);
         }
 
         public Dictionary<string,string> this[string Key,string Value]
         {
-            get => KeyValuePairs;
-            set => KeyValuePairs.Add(Key, Value);
+            get => RequestValue;
+            set => RequestValue.Add(Key, Value);
         }       
     }
 }
