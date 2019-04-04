a394fc0fba738aea0f0b80939c7b6f350f6520b4
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-04-04 18:18:41 +0500

    Edit IsActiveWindow

--- Mathematics/Vector/Vector2.cs
+++ Mathematics/Vector/Vector2.cs
@@ -25,11 +25,7 @@ namespace UIEngine.Mathematics.Vector
         /// </summary>
         /// <param name="x">The X component.</param>
         /// <param name="y">The Y component.</param>
-        public Vector2(float x, float y)
-        {
-            X = x;
-            Y = y;
-        }
+        public Vector2(float x, float y) => (X, Y) = (x, y);
         #endregion Constructors
 
         #region Public Static Properties
