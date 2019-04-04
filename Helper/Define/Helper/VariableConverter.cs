a95c8e6556780527e9c60b5f7b597fef6d996b60
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-04-04 18:14:55 +0500

    Class to convert bytes to custom types

---
+++ Helper/Define/Helper/VariableConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UIEngine.Helper.Define.Variable;
+
+namespace UIEngine.Helper.Define.Helper
+{
+    /// <summary>
+    /// Преобразует массив байтов в кастомные типы данных
+    /// </summary>
+    internal class VariableConverter
+    {
+        /// <summary>
+        /// Конвертирует массив байтов в тип DWORD
+        /// </summary>
+        /// <param name="Buffer">Массив байтов</param>
+        /// <returns></returns>
+        public static DWORD ToDWORD(byte[] Buffer) => new DWORD(BitConverter.ToUInt32(Buffer, 0));
+
+        /// <summary>
+        /// Конвертирует массив байтов в тип BOOLEAN
+        /// </summary>
+        /// <param name="Buffer">Массив байтов</param>
+        /// <returns></returns>
+        public static BOOLEAN ToBOOLEAN(byte[] Buffer) => new BOOLEAN(BitConverter.ToBoolean(Buffer, 0));
+    }
+}
