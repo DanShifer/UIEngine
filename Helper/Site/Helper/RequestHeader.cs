f2bd7594a073185eb821b70bc28021cb11c49fab
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-04-25 21:20:35 +0500

    Удалил ненужный индексатор

--- Helper/Site/Helper/RequestHeader.cs
+++ Helper/Site/Helper/RequestHeader.cs
@@ -11,11 +11,5 @@ namespace UIEngine.Helper.Site.Helper
             get => RequestValue[Key];
             set => RequestValue.Add(Key, value);
         }
-
-        public Dictionary<string,string> this[string Key,string Value]
-        {
-            get => RequestValue;
-            set => RequestValue.Add(Key, Value);
-        }       
     }
 }
