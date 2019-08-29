2e817709e8dcce3a22ffe2be3b09be7eed970ef1
Wolf <43772884+DanShifer@users.noreply.github.com>
2019-08-29 16:30:39 +0500

    Delete functions

--- Helper/Funct.cs
+++ Helper/Funct.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Text;
+using System.Windows.Forms;
 using UIEngine.API;
 using UIEngine.Helper.Define.Variable;
 using UIEngine.Memory.Helper;
@@ -9,29 +12,6 @@ namespace UIEngine.Helper
 {
     public static class Funct
     {
-        /// <summary>
-        /// Проверка активного окна
-        /// </summary>
-        /// <param name="ProcessName">Имя процесса</param>
-        /// <returns></returns>
-        public static BOOLEAN IsActiveWindow(string ProcessName) => HMemory.GetProcess(ProcessName).MainWindowHandle == UserAPI.GetForegroundWindow() ? true : false;
-
-        /// <summary>
-        /// Чтение смещений в файле
-        /// </summary>
-        /// <param name="Section">Секция</param>
-        /// <param name="Key">Ключ</param>
-        /// <param name="FileInfo">Имя файла</param>
-        /// <returns></returns>
-        public static int GetReadOffset(string Section, string Key, FileInfo FileInfo)
-        {
-            StringBuilder Offset = new StringBuilder(255);
-
-            KernelAPI.GetPrivateProfileString(Section, Key, "", Offset, 255, FileInfo.FullName);
-
-            return int.Parse(Offset.ToString());
-        }
-
         /// <summary>
         /// Рандомное число (Используется в основном для анимации ножей)
         /// </summary>
